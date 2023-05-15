30d900a50e03a12871b01e167ab2d12c24074ac8
Cristian Funes <cristianfunes79@gmail.com>
2023-05-15 19:56:01 -0300

    Se agrego documentacion.

--- inc/leds.h
+++ inc/leds.h
@@ -4,11 +4,29 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/// @brief Inicializa el driver de leds.
+/// @param direccion Direccion de memoria del driver.
 void leds_init(uint16_t* direccion);
+
+/// @brief Enciende un led.
+/// @param led Indice del led a encender.
 void leds_turn_on_single(int led);
+
+/// @brief Apaga un led.
+/// @param led Indice del led a apagar.
 void leds_turn_off_single(int led);
+
+/// @brief Enciende todos los leds
+/// @param  None
 void leds_turn_on_all(void);
+
+/// @brief Apaga todos los leds.
+/// @param  None
 void leds_turn_off_all(void);
+
+/// @brief Retorna el estado actual de un led.
+/// @param led Indice del led a leer.
+/// @return True si el led se encuentra encendido. False si el led se encuentra apagado.
 bool leds_get_state(int led);
 
 #endif
